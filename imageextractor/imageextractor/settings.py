36a66921eec8bd6c29f5630af5e1968719e00381
Abhinandan Wadhwa <awadhwa_be22@thapar.edu>
2022-12-12 01:18:03 +0530

    Allowing CORS

--- imageextractor/imageextractor/settings.py
+++ imageextractor/imageextractor/settings.py
@@ -36,13 +36,15 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    "corsheaders",
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'extractor.apps.ExtractorConfig',
-    'rest_framework'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -113,6 +115,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Static files (CSS, JavaScript, Images)
